64dd491be0dc1791adcb41626fefb98ba98c5bad
Byung Seok Kim <byung82@kiwiworks.kr>
2018-03-10 15:57:02 +0900

    kiwi 버전으로 수정

--- api_guardian.gemspec
+++ api_guardian.gemspec
@@ -17,9 +17,9 @@ Gem::Specification.new do |s|
   s.files = Dir['{app,config,db,lib}/**/*', 'MIT-LICENSE', 'Rakefile', 'README.md']
   s.test_files = Dir['spec/**/*']
 
-  s.required_ruby_version = '>= 2.2.2'
+  s.required_ruby_version = '>= 2.4.3'
 
-  s.add_dependency 'rails', '>= 5.0', '< 6.0'
+  s.add_dependency 'rails', '>= 5.1.5', '< 6.0'
   s.add_dependency 'active_model_otp', '~> 1.2'
   s.add_dependency 'active_model_serializers', '~> 0.10'
   # s.add_dependency 'acts_as_tenant', '~> 0.3'
@@ -30,11 +30,12 @@ Gem::Specification.new do |s|
   s.add_dependency 'kaminari', '~> 0.17'
   s.add_dependency 'koala', '~> 2.5'
   s.add_dependency 'paranoia', '~> 2.3'
-  s.add_dependency 'pg', '~> 0.18'
+  s.add_dependency 'pg', '~> 1.0.0'
   s.add_dependency 'phony', '~> 2.15'
   s.add_dependency 'pundit', '~> 1.1'
   s.add_dependency 'rack-cors', '~> 0.4'
   s.add_dependency 'zxcvbn-js', '~> 4.2'
+  s.add_dependency 'trailblazer-rails', '~> 2.1.0'
   s.add_development_dependency 'capybara', '~> 2.7'
   s.add_development_dependency 'database_cleaner', '~> 1.6'
   s.add_development_dependency 'factory_girl_rails', '~> 4.7'
--- app/controllers/api_guardian/api_controller.rb
+++ app/controllers/api_guardian/api_controller.rb
@@ -8,39 +8,39 @@ module ApiGuardian
     before_action :set_current_user
     before_action :prep_response
     before_action :validate_api_request
-    before_action :find_and_authorize_resource, except: [:index, :new, :create]
-    after_action :verify_policy_scoped, only: :index
-    after_action :verify_authorized, except: [:index]
+    # before_action :find_and_authorize_resource, except: [:index, :new, :create]
+    # after_action :verify_policy_scoped, only: :index
+    # after_action :verify_authorized, except: [:index]
     append_before_action :set_current_request
 
     rescue_from Exception, with: :api_error_handler
 
     attr_reader :current_user
 
-    def index
-      @resources = should_paginate? ? resource_store.paginate(page_params[:number], page_params[:size]) : resource_store.all
-      render json: @resources, include: includes
-    end
+    # def index
+    #   @resources = should_paginate? ? resource_store.paginate(page_params[:number], page_params[:size]) : resource_store.all
+    #   render json: @resources, include: includes
+    # end
 
-    def show
-      render json: @resource, include: includes
-    end
+    # def show
+    #   render json: @resource, include: includes
+    # end
 
-    def create
-      authorize resource_class
-      @resource = resource_store.create(create_resource_params)
-      render json: @resource, status: :created, include: includes
-    end
+    # def create
+    #   authorize resource_class
+    #   @resource = resource_store.create(create_resource_params)
+    #   render json: @resource, status: :created, include: includes
+    # end
 
-    def update
-      @resource = resource_store.update(@resource, update_resource_params)
-      render json: @resource, include: includes
-    end
+    # def update
+    #   @resource = resource_store.update(@resource, update_resource_params)
+    #   render json: @resource, include: includes
+    # end
 
-    def destroy
-      @resource.destroy!
-      head :no_content
-    end
+    # def destroy
+    #   @resource.destroy!
+    #   head :no_content
+    # end
 
     protected
 
